f618a3fcd38866a39b97ca6378f32af28fa93591
i-rd <randalchukzwilson@gmail.com>
2026-06-27 12:31:18 +0100

    fixed stuff 2

--- components/LogoutButton/LogoutButton.tsx
+++ components/LogoutButton/LogoutButton.tsx
@@ -9,7 +9,7 @@ export const LogoutButton = () => {
   return (
     <Button
       onClick={() => signOut({ callbackUrl: '/signin' })}
-      className="w-full p-2 flex items-center gap-2 text-sm font-bold text-white hover:bg-gray-300 hover:text-red-500 transition-colors group"
+      className="w-full p-2 flex items-center gap-2 text-sm font-bold text-white hover:text-red-500 transition-colors group"
     >
       <LogOut className="w-4 h-4  group-hover:translate-x-0.5 transition-transform" />
       <span className="uppercase tracking-widest text-xs">Log Out</span>
